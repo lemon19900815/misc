8c14f07b314745c93064eaee2e33558715e7c6eb
buerjia <zhiyuxingying@sina.com>
2022-02-22 16:03:16 +0800

    Update sort.h

--- algorithm/sort.h
+++ algorithm/sort.h
@@ -1,7 +1,10 @@
-#include <iostream>
+#ifndef __SORT_H__
+#define __SORT_H__
 
 void swap(int& a, int& b);
 
 void insertSort(int arr[], int len);
 
 void bubbleSort(int arr[], int len);
+
+#endif // __SORT_H__
