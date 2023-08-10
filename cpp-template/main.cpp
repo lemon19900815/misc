503fb187a6863db3c3c14cc8d7d4bdc0a3355a24
xie.gang <xiegang2@maccura.com>
2023-08-10 15:39:15 +0800

    learn template

--- cpp-template/main.cpp
+++ cpp-template/main.cpp
@@ -7,49 +7,155 @@
 using namespace std::string_literals;
 using namespace std::chrono_literals;
 
+using namespace meta;
+
 int main(int argc, char** argv)
 {
-    std::cout << "meta::add<1, 2>::value = "
-              << meta::add<1, 2>::value << std::endl;
-
-    struct st_foo
+    std::cout << "add<1, 2>::value = "
+              << add<1, 2>::value << std::endl;
     {
-        void foo() {}
-    };
+        struct st_foo
+        {
+            void foo() {}
+        };
 
-    constexpr auto int_has_foo = meta::has_member_func_foo<int>::value;
-    std::cout << "int " << (int_has_foo ? "" : "not")
-              << " have member foo" << std::endl;
+        constexpr auto int_has_foo = has_member_func_foo<int>::value;
+        std::cout << "int " << (int_has_foo ? "" : "not")
+                  << " have member foo" << std::endl;
 
-    constexpr auto st_has_foo = meta::has_member_func_foo<st_foo>::value;
-    std::cout << "st_foo " << (st_has_foo ? "" : "not")
-              << " have member foo" << std::endl;
+        constexpr auto st_has_foo = has_member_func_foo<st_foo>::value;
+        std::cout << "st_foo " << (st_has_foo ? "" : "not")
+                  << " have member foo" << std::endl;
 
-    // SFINAE(substitude failure is not an error)，
-    // 可以通过enable_if来实现编译期的if-else逻辑
-    std::cout << "meta::call_func(1) = " << meta::call_func(1) << std::endl;
-    std::cout << "meta::call_func('lemon's) = "
-              << meta::call_func("lemon"s) << std::endl;
+        // SFINAE(substitude failure is not an error)，
+        // 可以通过enable_if来实现编译期的if-else逻辑
+        std::cout << "call_func(1) = "
+                  << call_func(1) << std::endl;
 
-    auto str = "1"s;
-    auto xx = 1s;
+        std::cout << "call_func('lemon's) = "
+                  << call_func("lemon"s) << std::endl;
+    }
 
-    auto tp = std::make_tuple(1, 2, "lemon");
-    constexpr auto SIZE = std::tuple_size_v<decltype(tp)>;
-
-    // 遍历tuple
-    meta::for_each(
-        tp, std::make_index_sequence<SIZE>{},
-        [](auto& item, auto idx)
     {
-        std::cout << "idx = " << idx << ", value = " << item << std::endl;
-    });
+        auto str = "1"s;
+        auto xx = 1s;
 
-    std::cout << "meta::IntMax_v<sizeof(int), sizeof(char*), sizeof(double), sizeof(int64_t)> = "
-              << meta::IntMax_v<sizeof(int), sizeof(char*), sizeof(double), sizeof(int64_t)> << std::endl;
+        auto tp = std::make_tuple(1, 2, "lemon");
+        constexpr auto SIZE = std::tuple_size_v<decltype(tp)>;
 
-    std::cout << "meta::MaxAlign_v<int, char*, double, int64_t> = "
-              << meta::MaxAlign_v<int, char*, double, int64_t> << std::endl;
+        // 遍历tuple
+        for_each(
+            tp, std::make_index_sequence<SIZE>{},
+            [](auto& item, auto idx)
+        {
+            std::cout << "idx = " << idx << ", value = " << item << std::endl;
+        });
+    }
+
+    {
+        std::cout << "IntMax_v<sizeof(int), sizeof(char*), sizeof(double), sizeof(int64_t)> = "
+                  << IntMax_v<sizeof(int), sizeof(char*), sizeof(double), sizeof(int64_t)> << std::endl;
+
+        std::cout << "MaxAlign_v<int, char*, double, int64_t> = "
+                  << MaxAlign_v<int, char*, double, int64_t> << std::endl;
+    }
+
+    {
+        std::cout << "Contains_v<int, char*, double, int64_t> = "
+                  << std::boolalpha
+                  << Contains_v<int, char*, double, int64_t>
+                  << std::endl;
+
+        std::cout << "Contains_v<int, char*, double, int64_t, int32_t> = "
+                  << std::boolalpha
+                  << Contains_v<int, char*, double, int64_t, int32_t>
+                  << std::endl;
+    }
+
+    {
+        // 注意IndexOf模板的问题，借助Contains模板解决
+        std::cout << "IndexOf_v<int, char*, double, int64_t, int32_t> = "
+                  << IndexOf_v<int, char*, double, int64_t, int32_t>
+                  << std::endl;
+
+        std::cout << "IndexOf_v<int, char,char*,double,int64_t> = "
+                  << IndexOf_v<int, char, char*, double, int64_t>
+                  << std::endl;
+    }
+
+    {
+        std::cout << "At_t<1, int, char, double, int64_t> = "
+                  << typeid(At_t<1, int, char, double, int64_t>).name()
+                  << std::endl;
+
+        std::cout << "At_t<3, int, char, double, int64_t> = "
+                  << typeid(At_t<3, int, char, double, int64_t>).name()
+                  << std::endl;
+
+        // 编译错误：不能匹配到索引类型，超出了参数列表的个数
+        //std::cout << "At_t<4, int, char, double, int64_t> = "
+        //          << typeid(At_t<4, int, char, double, int64_t>{}).name()
+        //          << std::endl;
+    }
+
+    {
+        // use initializer_list and () expand param list.
+        print_for_each<int, double, char, int64_t>();
+    }
+
+    {
+        print_args(1, 3.14, 20L, 30LL);
+    }
+
+    {
+        std::function<int(int)> f = [](int a) {return a; };
+        auto f2 = [](int a) {return a; };
+        print_type<function_traits<int(int)>::fn_type>("function_traits<int(int)>::fn_type = ");
+        print_type<function_traits<int(int)>::args<0>::type>("function_traits<int(int)>::args<0>::type = ");
+        print_type<function_traits<decltype(f)>::fn_type>("function_traits<decltype(f)>::fn_type = ");
+        print_type<function_traits<decltype(f2)>::fn_type>("function_traits<decltype(f2)>::fn_type = ");
+
+        struct MyTest {
+            int operator()() { return 1; }
+            void foo(int ) { }
+        };
+
+        print_type<function_traits<decltype(&MyTest::foo)>::fn_type>("function_traits<decltype(&MyTest::foo) = ");
+        print_type<function_traits<MyTest>::fn_type>("function_traits<decltype(MyTest) = ");
+    }
+
+    {
+        struct MyTest {
+            int a;
+            int b;
+            int c;
+        };
+
+        meta::Variant<int, double, char, int64_t> v(1);
+        std::cout << "v type = " << v.Type().name() << std::endl;
+
+        v = 20ll;
+        std::cout << "v type = " << v.Type().name() << std::endl;
+
+        v = 3.14;
+        std::cout << "v type = " << v.Type().name() << std::endl;
+
+        v = 'a';
+        std::cout << "v type = " << v.Type().name() << std::endl;
+
+        v.Visit([](int v) {
+            std::cout << "int: " << v << std::endl;
+        }, [](double v) {
+            std::cout << "double: " << v << std::endl;
+        }, [](char v) {
+            std::cout << "char: " << v << std::endl;
+        }, [](int64_t v) {
+            std::cout << "int64_t: " << v << std::endl;
+        });
+
+        std::cout << "v.IndexOf<double> = "
+                  << v.IndexOf<double>() << std::endl;
+    }
 
 #ifdef _WIN32
     system("pause");
--- cpp-template/meta_func.h
+++ cpp-template/meta_func.h
@@ -1,9 +1,11 @@
 ﻿#pragma once
 
 #include <type_traits>
+#include <typeindex>
 #include <string>
 #include <iostream>
 #include <tuple>
+#include <functional>
 
 // reference: https://www.cnblogs.com/qicosmos/p/4480460.html
 
@@ -48,7 +50,7 @@ typename std::enable_if_t<std::is_same_v<T, std::string>, int> call_func(T&& t)
 }
 
 // tuple，c++17
-template<typename...Args, typename F, size_t... Idx>
+template<typename... Args, typename F, size_t... Idx>
 void for_each(const std::tuple<Args...>& tp, std::index_sequence<Idx...>, F&& f)
 {
     (std::forward<F>(f)(std::get<Idx>(tp), std::integral_constant<size_t, Idx> {}),...);
@@ -74,10 +76,320 @@ struct IntMax<v1, v2, rest...> : std::integral_constant<size_t,
     v1 >= v2 ? IntMax_v<v1, rest...> : IntMax_v<v2, rest...>> {};
 
 // 内存对齐
-template<typename...Args>
+template<typename... Args>
 struct MaxAlign : std::integral_constant<size_t, IntMax_v<sizeof(Args)...>> {};
 
-template<typename...Args>
+template<typename... Args>
 constexpr auto MaxAlign_v = MaxAlign<Args...>::value;
+
+// 参数类型是否包含指定类型
+template<typename T, typename... Args>
+struct Contains;
+
+template<typename T, typename Head, typename... Rest>
+struct Contains<T, Head, Rest...> :
+    std::conditional_t<std::is_same_v<T, Head>, std::true_type, Contains<T, Rest...>> {};
+
+template<typename T>
+struct Contains<T> : std::false_type {};
+
+template<typename T, typename...Args>
+constexpr auto Contains_v = Contains<T, Args...>::value;
+
+// 参数列表的索引：**这个模板函数是有问题的**
+// 参数在展开的过程中首先匹配模板IndexOf<T, Head, Args...>，IndexOf<T, Args...>::value+1;
+// 当匹配到最后成功0或者未匹配到-1时，依次往上递归，
+// IndexOf<int, char,char*,int,int64>：2
+// IndexOf<int, char,char*,double,int64>：3 错误，结果应为-1
+// 使用第2个Index_v模板，借助Contains模板来完成IndexOf的工作
+template<typename... Args>
+struct IndexOf;
+
+template<typename T>
+struct IndexOf<T>
+{
+    static constexpr auto value = -1;
+};
+
+template<typename T, typename... Args>
+struct IndexOf<T, T, Args...>
+{
+    static constexpr auto value = 0;
+};
+
+template<typename T, typename Head, typename... Args>
+struct IndexOf<T, Head, Args...>
+{
+    static constexpr auto value = IndexOf<T, Args...>::value + 1;
+};
+
+//error：如果不含T类型，结果是错误的
+//template<typename T, typename...Args>
+//constexpr auto IndexOf_v = IndexOf<T, Args...>::value;
+
+template<typename T, typename... Args>
+constexpr auto IndexOf_v = std::conditional_t<Contains_v<T, Args...>,
+    IndexOf<T, Args...>, std::integral_constant<int32_t, -1>>::value;
+
+// 编译器查找类型
+template<int Idx, typename... Args>
+struct At;
+
+template<int Idx, typename Head, typename... Args>
+struct At<Idx, Head, Args...>
+{
+    using type = typename At<Idx - 1, Args...>::type;
+};
+
+template<typename Head, typename... Args>
+struct At<0, Head, Args...>
+{
+    using type = Head;
+};
+
+template<size_t Idx, typename... Args>
+using At_t = typename At<Idx, Args...>::type;
+
+
+template<typename T>
+void print_type(std::string prefix="")
+{
+    std::cout << prefix << typeid(T).name() << std::endl;
+}
+
+template<typename... Args>
+void print_for_each()
+{
+    std::initializer_list<int>{ (print_type<Args>(), 0)... };
+}
+
+// 定义整型序列
+template<int...>
+struct IndexSeq {};
+
+// 生产整型序列
+template<int N, int... Seq>
+struct MakeSequence : MakeSequence<N - 1, N - 1, Seq...> {};
+
+template<int... Seq>
+struct MakeSequence<0, Seq...> {
+    using type = IndexSeq<Seq...>;
+};
+
+template<typename T, typename... Args>
+void print_arg(T t, Args... args)
+{
+    std::cout << typeid(T).name() << " = " << t << std::endl;
+    if constexpr (sizeof...(Args) == 0) {
+    } else {
+        print_arg(args...);
+    }
+}
+
+template<int... Seq, typename... Args>
+void print_helper(IndexSeq<Seq...>, std::tuple<Args...>&& tp)
+{
+    // 2.再将tuple转换为可变参数模板，将参数还原回来，再调用print
+    print_arg(std::get<Seq>(tp)...);
+}
+
+template<typename... Args>
+void print_args(Args... args)
+{
+    // 1.先将可变模板参数保存到tuple中
+    print_helper(typename MakeSequence<sizeof...(Args)>::type{}, std::make_tuple(args...));
+}
+
+///////////////////////////function_traits/////////////////////////////////////
+
+template<typename T>
+struct function_traits;
+
+// 普通函数
+template<typename Ret, typename... Args>
+struct function_traits<Ret(Args...)>
+{
+    // enum { arity = sizeof...(Args) }
+    static constexpr auto arity = sizeof...(Args);
+
+    using return_type = Ret;
+    using fn_type = return_type(Args...);
+    using stl_fn_type = std::function<fn_type>;
+
+    using fn_pointer = return_type(*)(Args...);
+
+    template<size_t I>
+    struct args
+    {
+        static_assert(I < arity, "index is out of range, index must less than sizeof Args");
+        using type = typename std::tuple_element<I, std::tuple<Args...>>::type;
+    };
+};
+
+// 函数指针
+template<typename Ret, typename... Args>
+struct function_traits<Ret(*)(Args...)> : function_traits<Ret(Args...)> {};
+
+// std::function
+template<typename Ret, typename... Args>
+struct function_traits<std::function<Ret(Args...)>> : function_traits<Ret(Args...)> {};
+
+// member function
+#define FUNCTION_TRAITS(...)\
+    template<typename Ret, typename Cls, typename... Args>\
+    struct function_traits<Ret(Cls::*)(Args...) __VA_ARGS__> : function_traits<Ret(Args...)> {};
+
+FUNCTION_TRAITS()
+FUNCTION_TRAITS(const)
+FUNCTION_TRAITS(volatile)
+FUNCTION_TRAITS(const volatile)
+
+// 函数对象
+template<typename Callable>
+struct function_traits : function_traits<decltype(&Callable::operator())> {};
+
+template<typename Function>
+typename function_traits<Function>::stl_fn_type to_function(Function&& lbd) {
+    return static_cast<typename function_traits<Function>::stl_fn_type>(std::forward<Function>(lbd));
+}
+
+template<typename Function>
+typename function_traits<Function>::fn_pointer to_function(Function&& lbd) {
+    return static_cast<typename function_traits<Function>::fn_pointer>(std::forward<Function>(lbd));
+}
+
+///////////////////////////function_traits/////////////////////////////////////
+
+template<typename... Types>
+class Variant
+{
+    static constexpr auto data_size = IntMax_v<sizeof...(Types)>;
+    static constexpr auto align_size = MaxAlign_v<Types...>;
+
+    using data_type = typename std::aligned_storage_t<data_size, align_size>;
+
+public:
+    template<int index>
+    using index_type = At_t<index, Types...>;
+
+    Variant(void) : type_idx_(typeid(void)) {
+    }
+    ~Variant() {
+        Destory(type_idx_, &data_);
+    }
+
+    Variant(Variant&& rhs) : type_idx_(rhs.type_idx_) {
+        Move0(rhs.type_idx_, &rhs.data_, &data_);
+    }
+
+    Variant(const Variant& rhs) : type_idx_(rhs.type_idx_) {
+        Copy0(rhs.type_idx_, &rhs.data_, &data_);
+    }
+
+    template<typename T,
+        typename = typename std::enable_if_t<Contains_v<std::remove_cv_t<T>, Types...>>>
+    Variant(T&& value) : type_idx_(typeid(void)) {
+        Destory(type_idx_, &data_);
+
+        using U = std::remove_cv_t<T>;
+
+        type_idx_ = std::type_index(typeid(U));
+        new (&data_) U(std::forward<T>(value));
+    }
+
+    template<typename T,
+        typename = typename std::enable_if_t<Contains_v<std::remove_cv_t<T>, Types...>>>
+    Variant& operator=(T&& value) {
+        Destory(type_idx_, &data_);
+
+        using U = std::remove_cv_t<T>;
+
+        type_idx_ = std::type_index(typeid(U));
+        new (&data_) U(std::forward<T>(value));
+        return *this;
+    }
+
+    template<typename T>
+    std::decay_t<T> Get() {
+        using U = std::decay_t<T>;
+        if (!Is<U>()) {
+            std::cout << typeid(U).name()
+                      << " is not defined, current type = "
+                      << type_idx_.name() << std::endl;
+            throw std::bad_cast{};
+        }
+
+        return *(U*)(&data_);
+    }
+
+    template<typename F, typename... Rest>
+    void Visit(F&& f, Rest&&... rest) {
+        using U = typename function_traits<F>::template args<0>::type;
+        if (Is<U>()) {
+            f(Get<U>());
+        } else if constexpr(sizeof...(rest) != 0) {
+            Visit(std::forward<Rest>(rest)...);
+        } else { }
+    }
+
+    bool IsEmpty() {
+        return Is<void>();
+    }
+
+    std::type_index Type() {
+        return type_idx_;
+    }
+
+    template<typename T>
+    int32_t IndexOf() {
+        return IndexOf_v<T, Types...>;
+    }
+
+private:
+    template<typename T>
+    bool Is() { return Is<T>(type_idx_); }
+
+    template<typename T>
+    bool Is(const std::type_index& id) {
+        return id == std::type_index(typeid(T));
+    }
+
+    void Destory(const std::type_index& id, void* buf) {
+        std::initializer_list<int> { (Destory0<Types>(id, buf), 0)... };
+    }
+
+    template<typename T>
+    void Destory0(const std::type_index& id, void* data) {
+        if (Is<T>(id)) {
+            reinterpret_cast<T*>(data)->~T();
+        }
+    }
+
+    void Move(const std::type_index& old_id, void* old_v, void* new_v) {
+        std::initializer_list<int> { (Move0<Types>(old_id, old_v, new_v), 0)... };
+    }
+
+    template<typename T>
+    void Move0(const std::type_index& old_id, void* old_v, void* new_v) {
+        if (Is<T>(old_id)) {
+            new (new_v) T(std::move(*reinterpret_cast<T*>(old_v)));
+        }
+    }
+
+    void Copy(const std::type_index& old_id, void* old_v, void* new_v) {
+        std::initializer_list<int> { (Copy0<Types>(old_id, old_v, new_v), 0)... };
+    }
+
+    template<typename T>
+    void Copy0(const std::type_index& old_id, void* old_v, void* new_v) {
+        if (Is<T>(old_id)) {
+            new (new_v) T(*reinterpret_cast<T*>(old_v));
+        }
+    }
+
+private:
+    data_type data_;
+    std::type_index type_idx_{ typeid(void) }; // 类型id
+};
 
 } // namespace meta
